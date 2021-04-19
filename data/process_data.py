3c2ee7f5252fc5497e7fe614b5aaf17e93aee7f0
Kshitij Jagatkar <kshitijjagatkar505@gmail.com>
2021-04-20 00:33:18 +0530

    Update: bugfix
    Added path for databse_filepath

--- data/process_data.py
+++ data/process_data.py
@@ -5,7 +5,6 @@ import sys
 
 
 def load_data(messages_filepath, categories_filepath):
-
     """
     Input
     :param messages_filepath:
@@ -13,63 +12,63 @@ def load_data(messages_filepath, categories_filepath):
         This function reads these both data sets and stores in each varibales
     :return: & returns merged dataset
     """
-    
-    messages = pd.read_csv(messages_filepath)   # load messages dataset
+
+    messages = pd.read_csv(messages_filepath)  # load messages dataset
     categories = pd.read_csv(categories_filepath)  # load categories dataset
-    
-    return messages.merge(categories, on = ['id'])   # returned merge datasets
+
+    return messages.merge(categories, on=['id'])  # returned merge datasets
 
 
 def clean_data(df):
-
     """
     Input
     :param df: takes merged data frame as an input
         this function performs tasks as clean, concatinate, drop duplicates
     :return: it returns cleaned new data frame.
     """
-    
-    categories = df['categories'].str.split(';',expand=True)  # create a dataframe of the 36 individual category columns
-    
-    row = categories.loc[0]    # select the first row of the categories dataframe
-    
+
+    categories = df['categories'].str.split(';',
+                                            expand=True)  # create a dataframe of the 36 individual category columns
+
+    row = categories.loc[0]  # select the first row of the categories dataframe
+
     # use this row to extract a list of new column names for categories.
-    category_colnames = categories.loc[0].apply(lambda x : x.split('-')[0]).to_list()
-    categories.columns = category_colnames   # rename the columns of `categories
-    
-    #Convert category values to just numbers 0 or 1
+    category_colnames = categories.loc[0].apply(lambda x: x.split('-')[0]).to_list()
+    categories.columns = category_colnames  # rename the columns of `categories
+
+    # Convert category values to just numbers 0 or 1
     for column in categories:
         # set each value to be the last character of the string
         categories[column] = categories[column].astype(str).apply(lambda x: x.split('-')[1])
         # convert column from string to numeric
         categories[column] = pd.to_numeric(categories[column])
-        
-    
+
     # Removing ambiguous values in the cleaned dataset( eg. df['related'] column has 3 unique values [0,1,2])
     # So we are replacing with with only two(0,1) values cause rest columns has only these values.
     for col in categories.columns:
-        print(categories[col].unique(),end="")
-    categories.related.replace(2,1,inplace=True)
+        print(categories[col].unique(), end="")
+    categories.related.replace(2, 1, inplace=True)
     print("\n")
-    print("\n after replacing 2 by 1",categories.related.unique())
-    
-    #Replace categories column in df with new category columns.
-    #Drop the categories column from the df dataframe since it is no longer needed.
-    df.drop(columns = 'categories',inplace=True)
+    print("\n after replacing 2 by 1", categories.related.unique())
+
+    # Replace categories column in df with new category columns.
+    # Drop the categories column from the df dataframe since it is no longer needed.
+    df.drop(columns='categories', inplace=True)
     # concatenate the original dataframe with the new `categories` dataframe
-    df = pd.concat([df,categories], axis=1)
-    
+    df = pd.concat([df, categories], axis=1)
+
     # Remove duplicates.
     # check number of duplicates
-    print("Number of duplicates in the dataset:",(df.duplicated() == True).sum())
+    print("Number of duplicates in the dataset:", (df.duplicated() == True).sum())
     # drop duplicates
     df.drop_duplicates(inplace=True)
     # check number of duplicates
-    print("Number of duplicates After dropping:",(df.duplicated() == True).sum())
-    
-    return df 
-    
-def save_data(df):
+    print("Number of duplicates After dropping:", (df.duplicated() == True).sum())
+
+    return df
+
+
+def save_data(df, database_filepath):
     """
     Input
     :param df: Takes df as an input
@@ -77,12 +76,14 @@ def save_data(df):
         here we use sqlite database
     :return: It does not return anything
     """
-    
-    #Save the clean dataset into an sqlite database.
-    engine3 = create_engine('sqlite:///data/disaster_response.db')
-    df.to_sql('msgs_categories', engine3, if_exists="replace",index=False) 
+
+    # Save the clean dataset into an sqlite database.
+
+    engine3 = create_engine('sqlite:///{}'.format(database_filepath))
+    df.to_sql('msgs_categories', engine3, if_exists="replace", index=False)
 
     return None
+
 
 def main():
     if len(sys.argv) == 4:
@@ -93,23 +94,23 @@ def main():
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
-        
+
         print("\n")
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print("\n")
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df)
-        
+        save_data(df, database_filepath)
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
               'DisasterResponse.db')
 
 
